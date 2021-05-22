c8a5937681a018584bcc8d144900d94ae3a7f390
chrisloarryn <chrisloarryn@gmail.com>
2021-05-21 22:54:33 -0400

    refactor: :art: change names

--- data/socials.js
+++ data/socials.js
@@ -3,48 +3,41 @@ import {
   SiLinkedin,
   SiTwitter,
   SiTwitch,
-  SiYoutube,
+  // SiYoutube,
   SiDiscord
-} from 'react-icons/si';
+} from 'react-icons/si'
 
 export const github = {
   icon: SiGithub,
-  href: 'https://github.com/gregogun',
+  href: 'https://github.com/chrisloarryn',
   name: 'github',
   color: null
-};
+}
 
 export const linkedin = {
   icon: SiLinkedin,
   href: 'https://www.linkedin.com/in/greg-ogun/',
   name: 'linkedin',
   color: '#0A66C2'
-};
+}
 
 export const twitter = {
   icon: SiTwitter,
-  href: 'https://twitter.com/gregogun',
+  href: 'https://twitter.com/chrisloarryn',
   name: 'twitter',
   color: '#1DA1F2'
-};
+}
 
 export const twitch = {
   icon: SiTwitch,
-  href: 'https://www.twitch.tv/gregogun',
+  href: 'https://www.twitch.tv/chrisloarryn',
   name: 'twitch',
   color: '#9146FF'
-};
-
-export const youtube = {
-  icon: SiYoutube,
-  href: 'https://www.youtube.com/channel/UCQDUHOvgodaEqjHm9Ihn87w',
-  name: 'youtube',
-  color: '#FF0000'
-};
+}
 
 export const discord = {
   icon: SiDiscord,
-  href: 'https://discord.gg/NHCkzptb',
+  href: 'https://discord.gg/chrisloarryn',
   name: 'discord',
   color: '#5865F2'
-};
+}
